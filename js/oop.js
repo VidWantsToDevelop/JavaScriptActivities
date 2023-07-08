ff4093c7cf0df8004e10acdafe848b9e8ca666da
VidWantsToDevelop <davidboldyrev@Davids-MacBook-Pro.local>
2023-07-08 09:04:58 -0400

    Made it a bit more dynamic | Ouputs text in HTML

--- js/oop.js
+++ js/oop.js
@@ -1,3 +1,6 @@
+const output1 = document.getElementById('output1');
+const output2 = document.getElementById('output2');
+
 
 // That's it! Now on to Lab 8...
 // Create a JavaScript function that permits the creation of a hamburger object
@@ -25,46 +28,64 @@ function Hamburger(garnishes, cheese, sauce, pattyType, hasPickles, combo, madeW
     this.meat = meat;
     this.bunType = bunType;
 
-    this.cook = function() {
+
+    let el = null;
+
+    this.cook = function(output) {
         console.log("Cooking the hamburger");
+        output.innerHTML += "<p>Cooking the hamburger</p>";
         console.log(`A smell of fried ${this.meat} fills the air`);
+        output.innerHTML += `<p>A smell of fried ${this.meat} fills the air</p>`;
         console.log("Lovely song is playing on the background");
+        output.innerHTML += "<p>Lovely song is playing on the background</p>";
 
         setTimeout(() => {
             console.log("Hamburger is ready");
-            console.log(combo ? "Don't forget your fries and pop!" : "Enjoy your meal!");
+            output.innerHTML += "<p>Hamburger is ready</p>";
+            const comboState = combo ? "Don't forget your fries and pop!" : "Enjoy your meal!";
+            console.log(comboState);
+            output.innerHTML += `<p>${comboState}</p>`;
         }, 3000)
     }
 
-    this.eat = function() {
+    this.eat = function(output) {
         console.log("Taking a bite...");
+        output.innerHTML += "<p>Taking a bite...</p>";
 
         setTimeout(() => {
             if(this.madeWithLove) {
                 console.log("This is the best hamburger I've ever had!");
+                output.innerHTML += "<p>This is the best hamburger I've ever had!</p>";
             }
             else {
-                console.log(hasPickles ? "At least it has pickles..." : "This is the worst hamburger I've ever had!")
+                const picklesState = hasPickles ? "At least it has pickles..." : "This is the worst hamburger I've ever had!"
+                console.log(picklesState);
+                output.innerHTML += `<p>${picklesState}</p>`;
             }
         }, 3000);
     }
 
-    this.observe = function() {
+    this.observe = function(output) {
         console.log("Observing the hamburger...");
+        output.innerHTML += "<p>Observing the hamburger...</p>";
 
         setTimeout(() => {
             if(bunType) {
-                console.log(`I wouldn't expect ${this.name} to have such a tasy ${this.bunType} bun!`);
+                const observation = `I wouldn't expect ${this.name} to have such a tasy ${this.bunType} bun!`;
+                console.log(observation);
+                output.innerHTML += `<p>${observation}</p>`;
             }
             else {
                 console.log("That's the first time in my life when I get a hamburger without a bun...");
+                output.innerHTML += "<p>That's the first time in my life when I get a hamburger without a bun...</p>";
             }
         }, 3000);
     }
 
-    this.describe = function() {
+    this.describe = function(output) {
         let description = `Pretty "${this.name}" hamburger with the ${this.pattyType} ${this.meat} patty, dressed with ${this.sauce} sauce, with the ${this.cheese} cheese on top of the ${this.garnishes}`;
         console.log(description);
+        output.innerHTML += `<p>${description}</p>`;
     }
 }
 
@@ -73,25 +94,25 @@ var hamburger1 = new Hamburger("tomatoes", "two slices of", "hot", "tripple", tr
 var hamburger2 = new Hamburger("onions", "mozarello", "sweet and sour", "double", true, false, false, "FattyPatty", "fish");
 
 setTimeout(() => {
-    hamburger1.cook();
+    hamburger1.cook(output1);
 }, 3100);
 setTimeout(() => {
-    hamburger1.eat();
+    hamburger1.eat(output1);
 }, 6200);
 setTimeout(() => {
-    hamburger1.observe();
+    hamburger1.observe(output1);
 }, 9300);
 
 setTimeout(() => {
-    hamburger2.cook();
+    hamburger2.cook(output2);
 }, 12400);
 
 setTimeout(() => {
-    hamburger2.eat();
+    hamburger2.eat(output2);
 }, 15500);
 
 setTimeout(() => {
-    hamburger2.observe();
+    hamburger2.observe(output2);
 }, 18600);
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
